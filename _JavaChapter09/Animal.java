751124e46a395336223731e47149ad6b5ba2182c
Cameron <Liujmiracle@163.com>
2016-05-06 22:53:17 +0800

    Ok

--- _JavaChapter09/Animal.java
+++ _JavaChapter09/Animal.java
@@ -11,8 +11,10 @@ public abstract class Animal {
 
 	public Animal(String theName) {
 		name = theName;
+		name = theName; // no thing will happen
 	}
 	public Animal() {
 		super();
 	}
+	// Cameron add some thing
 }
